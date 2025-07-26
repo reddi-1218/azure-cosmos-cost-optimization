4e467f4683dd4df5f39c694aea99551aa5e5b6e8
reddi-1218 <reddichanikya1574@gmail.com>
2025-07-26 19:22:15 +0530

    read_api handler.py

--- read_api handler.py
+++ read_api handler.py
@@ -1,1 +1,26 @@
+import os, json
+import azure.functions as func
+from azure.cosmos import CosmosClient, exceptions
+from azure.storage.blob import BlobServiceClient
 
+cosmos = CosmosClient.from_connection_string(os.getenv("COSMOS_URI"))
+container = cosmos.get_database_client(os.getenv("COSMOS_DB")).get_container_client(os.getenv("COSMOS_CONTAINER"))
+blobs = BlobServiceClient.from_connection_string(os.getenv("BLOB_CONN")).get_container_client(os.getenv("BLOB_CONTAINER"))
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    record_id = req.route_params.get("id")
+    partition_key = req.headers.get("x-partition-key")
+    try:
+        rec = container.read_item(record_id, partition_key)
+        return func.HttpResponse(json.dumps(rec), mimetype="application/json")
+    except exceptions.CosmosResourceNotFoundError:
+        blob = blobs.get_blob_client(f"{record_id}.json")
+        if blob.exists():
+            return func.HttpResponse(blob.download_blob().readall(), mimetype="application/json")
+        return func.HttpResponse("Not Found", status_code=404)
+
+
+requirements.txt:
+azure-functions
+azure-cosmos
+azure-storage-blob
